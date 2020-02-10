493fbd5d29a9c43a8d83315aaa080a526d110d73
Ales7 <ales75016@gmail.com>
2020-02-10 20:03:35 +0100

    Corrected an error in the function descriptions

--- features/featuresAles.py
+++ features/featuresAles.py
@@ -14,16 +14,16 @@ def lenDiff (sen1, sen2):
     return len(sen1) - len(sen2)
 
 def find1 (sen1, sen2, vocab):
-    # takes 2 sentences represented each by a list of strings,
+    # takes 2 sentences and a vocabulary represented each by a list of strings,
     # returns the number of occurences of tokens of vocab in sen1 as an int
     return sum(el in vocab for el in sen1)
     
 def find2 (sen1, sen2, vocab):
-    # takes 2 sentences represented each by a list of strings,
+    # takes 2 sentences and a vocabulary represented each by a list of strings,
     # returns the number of occurences of tokens of vocab in sen2 as an int
     return sum(el in vocab for el in sen2)
 
 def findDiff (sen1, sen2, vocab):
-    # takes 2 sentences represented each by a list of strings,
+    # takes 2 sentences and a vocabulary represented each by a list of strings,
     # returns the difference between the number of occurences of tokens of vocab (sen1 - sen2) as an int
     return find1(sen1, sen2, vocab) - find2(sen1, sen2, vocab)
